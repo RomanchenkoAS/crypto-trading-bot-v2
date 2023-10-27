542753e8b773457ab75306d3d066c7a40dd553e0
RomanchenkoAS <RomanchenkoAS@yandex.ru>
2023-10-28 01:29:42 +0600

    scraper

--- scraping/scraper.py
+++ scraping/scraper.py
@@ -5,8 +5,16 @@ import plotly.graph_objects as go
 
 
 class Scraper:
-    def __init__(self, currency_pair="btcusd"):
-        self.currency_pair = currency_pair
-        self.url = f"https://www.bitstamp.net/api/v2/ohlc/{currency_pair}/"
-        self.df = None
+    def __init__(self, currency_pair: str = "btcusd"):
+        self.dates: list[int] = []
+        self.currency_pair: str = currency_pair
+        self.url: str = f"https://www.bitstamp.net/api/v2/ohlc/{currency_pair}/"
+        self.df: pd.DataFrame = pd.DataFrame()
+
+    def set_time_range(self, range_size: int, frequency: str = "1D"):
+        end = datetime.now()
+        start = datetime.now() - timedelta(range_size)
+
+        dates = pd.date_range(start, end, freq=frequency)
+        self.dates = [int(x.value / 10 ** 9) for x in list(dates)]
 
